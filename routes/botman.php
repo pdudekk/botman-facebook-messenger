308aca1160759b16ae7f7a55d8ae3eae43b44d5d
Pawel <paweldudekk@gazeta.pl>
2018-05-30 17:06:07 +0200

    bot setup

--- routes/botman.php
+++ routes/botman.php
@@ -9,4 +9,8 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!'.' '.$firstname);
 });
 
+$botman->on('messaging_optins', function($payload, $bot) {
+  $bot->reply('Hi');
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
