6b0e56c9c97b6d48bdbbeefe30ccd917e049c930
Pawel <paweldudekk@gazeta.pl>
2018-05-30 16:34:21 +0200

    bot setup

--- routes/botman.php
+++ routes/botman.php
@@ -6,8 +6,7 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
-$botman->hears('Hello', function (BotMan $bot) {
-           $userName = $bot->getUser()->getFirstName() . ' ' . $bot->getUser()->getLastName();
-           $bot->reply('Hi ' . $userName);
-});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->listen();
